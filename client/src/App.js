9f2d229de4217d569ec09377823b865a629c49ee
Cefalu Wave <luke.evan.graham@gmail.com>
2020-12-07 22:55:20 -0800

    getting ready to setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,27 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
+
+import Layout from "./hoc/Layout/Layout";
 import EventList from "./containers/EventList/EventList";
-import EventCreate from "./containers/EventCreate/EventCreate"
+import EventCreate from "./containers/EventCreate/EventCreate";
 import history from "./history";
 
 const App = (props) => {
+  let routes = (
+    <Switch>
+      <Route path="/" exact component={EventList} />
+      <Route path="/admin" component={EventCreate} />
+    </Switch>
+  );
+
   return (
     <div>
       <CssBaseline />
       <Router history={history}>
-        <div>
-          <Container maxWidth="md">
-            <h1>App</h1>
-            <Switch>
-              <Route path="/" exact component={EventList} />
-              <Route path="/admin" component={EventCreate} />
-            </Switch>
-          </Container>
-        </div>
+        <Container maxWidth="md">
+          <Layout>{routes}</Layout>
+        </Container>
       </Router>
     </div>
   );
---
+++ client/src/hoc/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Layout = (props) => (
+  <div>
+    <h2>RB Community Church | Seat Save</h2>
+    <main>{props.children}</main>
+  </div>
+);
+
+export default Layout;
