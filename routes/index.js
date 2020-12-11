464c69db7da8b6b8863e945aa1742d504b6da7c6
Cefalu Wave <luke.evan.graham@gmail.com>
2020-12-10 23:00:40 -0800

    keeping it at the latest

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.use("/event", eventRoutes)
 router.use("/group", groupRoutes)
 
 // For anything else, render the html page
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-  });
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+//   });
 
 module.exports = router;
