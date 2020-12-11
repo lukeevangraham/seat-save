0ad31fe7ad74083aef8d1d91a2145f07214536b9
Cefalu Wave <luke.evan.graham@gmail.com>
2020-12-10 16:47:51 -0800

    changing api port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan")
 const path = require("path");
 const mongoose = require("mongoose")
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 require("dotenv").config()
 
 const app = express();
